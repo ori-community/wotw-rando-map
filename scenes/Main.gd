f0983e3a8dd957eba22ba86a3676142de2b3e572
Mawe <MalteWerner.Service@outlook.de>
2026-02-06 22:05:56 +0100

    added time label

--- scenes/Main.gd
+++ scenes/Main.gd
@@ -6,6 +6,7 @@ extends Control
 @onready var speed_slider: HSlider = %SpeedSlider
 @onready var play_button: TextureButton = %PlayButton
 @onready var speed_label: Label = %SpeedLabel
+@onready var time_label: Label = %TimeLabel
 
 var is_playing = false:
 	set(value):
@@ -19,7 +20,9 @@ var sliders_gets_dragged = false
 
 func _ready() -> void:
 	speed_label.text = str(speed_slider.value, "x")
-	var save_file_reader := WotwSaveFileReader.new(FileAccess.get_file_as_bytes("C:/Users/Mawe/AppData/Local/Ori and the Will of The Wisps/saveFile1 - Copy.uberstate"))
+	update_time_label()
+	
+	var save_file_reader := WotwSaveFileReader.new(FileAccess.get_file_as_bytes("C:/Users/Mawe/AppData/Local/Ori and the Will of The Wisps/saveFile0.uberstate"))
 	var slot_data := save_file_reader.read_events_stream()
 	var events_stream_reader := WotwEventsStreamReader.new()
 	events_stream_reader.append_events(slot_data)
@@ -31,12 +34,15 @@ func _process(_delta: float) -> void:
 	if is_playing && !sliders_gets_dragged:
 		time_slider.value += _delta * speed_slider.value
 
-func _on_time_slider_value_changed(value: float) -> void:
-	events_view.slice_end_time = value
+func update_time_label() -> void:
+		time_label.text = TimeUtils.format_time(time_slider.value)
 
+func _on_time_slider_value_changed(value: float) -> void:
+	update_time_label()
+	events_view.slice_end_time = value
+	
 func _on_button_pressed() -> void:
 	is_playing = !is_playing
-
 
 func _on_time_slider_drag_started() -> void:
 	sliders_gets_dragged = true
@@ -44,10 +50,8 @@ func _on_time_slider_drag_started() -> void:
 func _on_time_slider_drag_ended(value_changed: bool) -> void:
 	sliders_gets_dragged = false
 
-
 func _on_speed_slider_value_changed(value: float) -> void:
 	speed_label.text = str(value, "x")
-
 
 func _on_fade_out_button_toggled(toggled_on: bool) -> void:
 	events_view.fade_out = toggled_on
---
+++ utils/TimeUtils.gd
@@ -0,0 +1,12 @@
+extends Node
+
+func format_time(value: float) -> String:
+	var total_seconds := int(value)
+	
+	var hours := int(total_seconds / 3600.0)
+	var minutes := (total_seconds % 3600) / 60.0
+	var seconds := total_seconds % 60
+	
+	var milliseconds := int((value - total_seconds) * 10)
+	
+	return "%02d:%02d:%02d.%d" % [hours, minutes, seconds, milliseconds]
