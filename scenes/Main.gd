b9900c12c6cfa7be04196dcce8c63dca53f6972e
Mawe <MalteWerner.Service@outlook.de>
2026-02-07 00:24:45 +0100

    modified layout of time container

--- scenes/Main.gd
+++ scenes/Main.gd
@@ -4,7 +4,7 @@ extends Control
 @onready var events_view: EventsView = %EventsView
 @onready var time_slider: HSlider = %TimeSlider
 @onready var speed_slider: HSlider = %SpeedSlider
-@onready var play_button: TextureButton = %PlayButton
+@onready var play_button: TextureButton = %ButtonPlay
 @onready var speed_label: Label = %SpeedLabel
 @onready var time_label: Label = %TimeLabel
 
@@ -20,7 +20,6 @@ var sliders_gets_dragged = false
 
 func _ready() -> void:
 	speed_label.text = str(speed_slider.value, "x")
-	update_time_label()
 	
 	var save_file_reader := WotwSaveFileReader.new(FileAccess.get_file_as_bytes("C:/Users/Mawe/AppData/Local/Ori and the Will of The Wisps/saveFile0.uberstate"))
 	var slot_data := save_file_reader.read_events_stream()
@@ -29,13 +28,16 @@ func _ready() -> void:
 	
 	events_view.stream = events_stream_reader.stream
 	time_slider.max_value = events_stream_reader.stream.in_game_time_end
+	update_time_label()
 
 func _process(_delta: float) -> void:
 	if is_playing && !sliders_gets_dragged:
 		time_slider.value += _delta * speed_slider.value
+		if time_slider.value >= time_slider.max_value:
+			is_playing = false
 
 func update_time_label() -> void:
-		time_label.text = TimeUtils.format_time(time_slider.value)
+	time_label.text = str(TimeUtils.format_time(time_slider.value), " / ", TimeUtils.format_time(time_slider.max_value))
 
 func _on_time_slider_value_changed(value: float) -> void:
 	update_time_label()
@@ -55,3 +57,9 @@ func _on_speed_slider_value_changed(value: float) -> void:
 
 func _on_fade_out_button_toggled(toggled_on: bool) -> void:
 	events_view.fade_out = toggled_on
+
+func _on_button_beginning_pressed() -> void:
+	time_slider.value = 0
+
+func _on_button_end_pressed() -> void:
+	time_slider.value = time_slider.max_value
